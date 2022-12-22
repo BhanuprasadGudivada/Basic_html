45abddfaae3b9b245dd954924a63e0247df33a17
BhanuprasadGudivada <bhanuprasad1154232@gmail.com>
2022-12-22 11:59:59 +0530

    first push

---
+++ script.js
@@ -0,0 +1,55 @@
+let InputElement=document.getElementById("InputElement") 
+function login(){
+    if ((InputElement.value === "bhanu@gmail.com") || (InputElement.value ==="user@gmail.com") || (InputElement.value === "demouser@gmail.com")){
+        alert("Valid User");
+        window.open("websitepage.html")
+    }
+    else{
+        alert("Invaild Username")
+    }
+}
+
+let HomePage = document.getElementById("HomePage")
+let ContactPage = document.getElementById("ContactPage")
+let PaymentPage = document.getElementById("PaymentPage")
+let AboutPage = document.getElementById("AboutPage")
+let DataLoaderPage = document.getElementById("DataLoaderPage")
+
+function Home(){
+    HomePage.classList.remove("d-none")
+    ContactPage.classList.add("d-none")
+    PaymentPage.classList.add("d-none")
+    AboutPage.classList.add("d-none")
+    DataLoaderPage.classList.add("d-none")
+}
+function Contact(){
+    HomePage.classList.add("d-none")
+    ContactPage.classList.remove("d-none")
+    PaymentPage.classList.add("d-none")
+    AboutPage.classList.add("d-none")
+    DataLoaderPage.classList.add("d-none")
+}
+function Payment(){
+    HomePage.classList.add("d-none")
+    ContactPage.classList.add("d-none")
+    PaymentPage.classList.remove("d-none")
+    AboutPage.classList.add("d-none")
+    DataLoaderPage.classList.add("d-none")
+}
+function About(){
+    HomePage.classList.add("d-none")
+    ContactPage.classList.add("d-none")
+    PaymentPage.classList.add("d-none")
+    AboutPage.classList.remove("d-none")
+    DataLoaderPage.classList.add("d-none")
+}
+function DataLoader(){
+    HomePage.classList.add("d-none")
+    ContactPage.classList.add("d-none")
+    PaymentPage.classList.add("d-none")
+    AboutPage.classList.add("d-none")
+    DataLoaderPage.classList.remove("d-none")
+}
+function Login(){
+    window.open("index.html")
+}
