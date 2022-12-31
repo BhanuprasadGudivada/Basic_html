6fcd6cae780ac1da5acb8c799046ba4ddf3521dd
BhanuprasadGudivada <bhanuprasad1154232@gmail.com>
2022-12-31 11:50:07 +0530

    contact

--- script.js
+++ script.js
@@ -69,7 +69,7 @@ function About(){
     //PaymentPage.classList.add("d-none")
     //AboutPage.classList.remove("d-none")
     //DataLoaderPage.classList.add("d-none")
-    window.location="aboutWeb.html"
+    window.location="aboutweb.html"
 }
 function DataLoader(){
     HomePage.classList.add("d-none")
