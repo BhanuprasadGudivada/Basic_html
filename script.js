f5fd42580c26e57cabdfcf504dd103c73e25e4e6
BhanuprasadGudivada <bhanuprasad1154232@gmail.com>
2023-05-18 22:20:44 +0530

    GTM

--- script.js
+++ script.js
@@ -15,11 +15,11 @@
   var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
 })(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-0UD7HOEJMZOK-2");
 
-(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+/*(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NNXNQK7');
+})(window,document,'script','dataLayer','GTM-NNXNQK7');*/
 
 
 
