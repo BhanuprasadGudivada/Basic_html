e315a9b7b049dcbf2d5c531b50633a1bd3c4e398
BhanuprasadGudivada <121215783+BhanuprasadGudivada@users.noreply.github.com>
2023-08-04 20:05:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
     (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
   var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
   var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-NV5EY6EHZX0S-2");
+})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-OULYAMSSGJ8M-2");
 
 
 (function(n, t, a, e, x) {
@@ -19,7 +19,7 @@
     let c = t.getElementsByTagName("script")[0];
     c.parentNode.insertBefore(r, c);
 })
-(window, document, "https://web-sdk.aptrinsic.com/api/aptrinsic.js", "AP-0UD7HOEJMZOK-2", {
+(window, document, "https://web-sdk.aptrinsic.com/api/aptrinsic.js", "AP-OULYAMSSGJ8M-2", {
     cssNonce: "nonce-r@ndom"
 });
 
