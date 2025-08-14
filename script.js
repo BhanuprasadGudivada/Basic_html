41739d8183c38a6f061f576d981e178e4159f63a
BhanuprasadGudivada <121215783+BhanuprasadGudivada@users.noreply.github.com>
2025-08-14 08:45:57 +0530

    Update script.js

--- script.js
+++ script.js
@@ -439,4 +439,19 @@ window.addEventListener('beforeunload', function() {
     });
 });
 
+document.getElementById('uniqueDropdownButton').addEventListener('click', function() {
+    document.getElementById('uniqueDropdownContent').classList.toggle('show');
+});
+
+window.onclick = function(event) {
+    if (!event.target.matches('.dropdown-button')) {
+        var dropdowns = document.getElementsByClassName('dropdown-content');
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
 
