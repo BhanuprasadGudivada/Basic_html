c5fb6221ce59faf6fb377bfd0d57ba5acfa37a7b
BhanuprasadGudivada <121215783+BhanuprasadGudivada@users.noreply.github.com>
2026-01-21 07:54:06 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,10 @@ var config= {kcAllowedFuncNames: ["zendeskchat", "initializeSearchBot"]};
     (n[i].q=n[i].q||[]).push(arguments)},n[i].p=e;n[i].c=co;
   var r=t.createElement("script");r.async=!0,r.src=a+"?a="+e;
   var c=t.getElementsByTagName("script")[0];c.parentNode.insertBefore(r,c)
-})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-OULYAMSSGJ8M-2", config);
+})(window,document,"https://web-sdk.aptrinsic.com/api/aptrinsic.js","AP-OULYAMSSGJ8M-2", config, {
+        badgesUseFixedPosition: true,
+    });
+);
 
 
 
